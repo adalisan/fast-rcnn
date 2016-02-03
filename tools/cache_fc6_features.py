2394df698afa391012094324b67dfe694fc0d5bc
Yu-Wei Chao <ywchao@umich.edu>
2016-02-03 00:51:36 -0500

    cache fc6 full ft + det ctx

--- tools/cache_fc6_features.py
+++ tools/cache_fc6_features.py
@@ -18,7 +18,8 @@ import cv2
 
 num_classes = 80
 blob_name_ip = 'data'
-blob_name_op = 'fc6'
+blob_name_op_reg = 'fc6'
+blob_name_op_ctx = 'pool6'
 w_reg = 227
 h_reg = 227
 w_ctx8 = 419
@@ -27,6 +28,13 @@ h_ctx8 = 419
 im_base = './external/hico_20150920/images/'
 anno_file = './external/hico_20150920/anno.mat'
 det_base = './caches/det_base_caffenet/'
+
+prototxt_reg = './models/cache_fc6_reg.prototxt'
+prototxt_ctx = './models/cache_fc6_ctx.prototxt'
+model_pre_reg = './external/fast-rcnn/data/imagenet_models/CaffeNet.v2.caffemodel'
+model_fto_reg = './data/data/finetuned_models/caffenet_train_object_single_iter_50000.caffemodel'
+model_ftv_reg = './data/data/finetuned_models/caffenet_train_verb_single_iter_50000.caffemodel'
+model_pre_ctx = './data/data/imagenet_models/CaffeNet.v2.conv.caffemodel'
 
 
 def parse_args():
@@ -60,13 +68,10 @@ def parse_args():
     parser.add_argument('--sid', type=int, required=True)
     parser.add_argument('--eid', type=int, required=True)
 
-    if len(sys.argv) == 1:
-        parser.print_help()
-        sys.exit(1)
-
+    # if len(sys.argv) == 1:
+    #     parser.print_help()
+    #     sys.exit(1)
     # args = parser.parse_args('--gpu 1 \
-    #                           --def ./dev_feat_extract/data/models/CaffeNet_18_horse/test.prototxt \
-    #                           --net ./external/fast-rcnn/data/imagenet_models/CaffeNet.v2.caffemodel \
     #                           --top_k 100 \
     #                           --sid 1 \
     #                           --eid 47774'.split())
@@ -132,7 +137,7 @@ def _get_blobs(im, rois, ctx_mode):
         im_blob = np.zeros((num_boxes, 3, h_reg, w_reg), dtype=np.float32)
         for ind in xrange(num_boxes):
             im_blob[ind,:,:,:] = _get_one_blob(im, rois[ind,:], w_reg, h_reg)
-    if ctx_mode == 'ctx8':
+    if ctx_mode == 'ctx':
         h_ori = im.shape[0]
         w_ori = im.shape[1]
         im_blob = np.zeros((num_boxes, 3, h_ctx8, w_ctx8), dtype=np.float32)
@@ -176,11 +181,6 @@ if __name__ == '__main__':
     print('Using config:')
     pprint.pprint(cfg)
 
-    caffe.set_mode_gpu()
-    caffe.set_device(args.gpu_id)
-    net = caffe.Net(args.prototxt, args.caffemodel, caffe.TEST)
-    net.name = os.path.splitext(os.path.basename(args.caffemodel))[0]
-
     # get args
     top_k = args.top_k
     sid = args.sid
@@ -188,15 +188,19 @@ if __name__ == '__main__':
 
     # set output path
     feat_base = './caches/cache' + \
-                '_' + blob_name_op + \
+                '_' + blob_name_op_reg + \
                 '_' + '{:04d}'.format(top_k) + \
                 '/'
+    # make directories
+    if not os.path.exists(feat_base + 'train2015'):
+         os.makedirs(feat_base + 'train2015')
+    if not os.path.exists(feat_base + 'test2015'):
+         os.makedirs(feat_base + 'test2015')
 
     # load images
     lsim_tr = sio.loadmat(anno_file)['list_train']
     lsim_ts = sio.loadmat(anno_file)['list_test']
     lsim = np.vstack((lsim_tr,lsim_ts))
-
     image_index = [str(im[0][0]) for im in lsim]
     num_images = len(image_index)
 
@@ -204,7 +208,20 @@ if __name__ == '__main__':
     assert(sid >= 0 and eid <= num_images)
 
     # timers
-    _t = {'total' : Timer(), 'full' : Timer(), 'det_reg' : Timer()}
+    _t = {'total' : Timer(), 'full' : Timer(), 'det' : Timer()}
+
+    # set device for caffe
+    caffe.set_mode_gpu()
+    caffe.set_device(args.gpu_id)
+    # load networks
+    net_pre_reg = caffe.Net(prototxt_reg, model_pre_reg, caffe.TEST)
+    net_pre_ctx = caffe.Net(prototxt_ctx, model_pre_ctx, caffe.TEST)
+    net_fto_reg = caffe.Net(prototxt_reg, model_fto_reg, caffe.TEST)
+    net_ftv_reg = caffe.Net(prototxt_reg, model_ftv_reg, caffe.TEST)
+    net_pre_reg.name = os.path.splitext(os.path.basename(model_pre_reg))[0]
+    net_pre_ctx.name = os.path.splitext(os.path.basename(model_pre_ctx))[0]
+    net_fto_reg.name = os.path.splitext(os.path.basename(model_fto_reg))[0]
+    net_ftv_reg.name = os.path.splitext(os.path.basename(model_ftv_reg))[0]
 
     for i in xrange(sid-1, eid):
         # set directories
@@ -215,10 +232,6 @@ if __name__ == '__main__':
         im_dir = im_base + image_set
         det_dir = det_base + image_set
         feat_dir = feat_base + image_set
-
-        # make directories
-        if not os.path.exists(feat_dir):
-             os.makedirs(feat_dir)
 
         # get file names
         im_file = os.path.join(im_dir, image_index[i]);
@@ -243,19 +256,29 @@ if __name__ == '__main__':
         w_ori = im.shape[1]
         box_f = np.array((0,0,w_ori-1,h_ori-1), dtype='uint16')
         blobs = {blob_name_ip : _get_one_blob(im, box_f, w_reg, h_reg)[None, :]}
-        net.blobs[blob_name_ip].reshape(*(blobs[blob_name_ip].shape))
-        blobs_out = net.forward(**(blobs))
-        feat_full = np.copy(blobs_out[blob_name_op])
+        # extract feature with ImageNet model
+        net_pre_reg.blobs[blob_name_ip].reshape(*(blobs[blob_name_ip].shape))
+        blobs_out = net_pre_reg.forward(**(blobs))
+        feat_full_pre = np.copy(blobs_out[blob_name_op_reg])
+        # extract feature with finetuned O model
+        net_fto_reg.blobs[blob_name_ip].reshape(*(blobs[blob_name_ip].shape))
+        blobs_out = net_fto_reg.forward(**(blobs))
+        feat_full_fto = np.copy(blobs_out[blob_name_op_reg])
+        # extract feature with finetuned V model
+        net_ftv_reg.blobs[blob_name_ip].reshape(*(blobs[blob_name_ip].shape))
+        blobs_out = net_ftv_reg.forward(**(blobs))
+        feat_full_ftv = np.copy(blobs_out[blob_name_op_reg])
         _t['full'].tic()
 
         # detection reg feature
-        _t['det_reg'].tic()
+        _t['det'].tic()
         assert os.path.exists(det_file), \
                'Detection file not found at: {}'.format(det_file)
         res = sio.loadmat(det_file)
         assert res['dets'].shape[1] == num_classes+1, \
                'Incorrect number of classes in {}'.format(det_file)
-        feat_det_reg = np.empty(num_classes, dtype=object)
+        feat_det_pre_reg = np.empty(num_classes, dtype=object)
+        feat_det_pre_ctx = np.empty(num_classes, dtype=object)
         # exclude background class: start index from 1
         for j in xrange(1, num_classes+1):
             # get detection boxes
@@ -264,23 +287,28 @@ if __name__ == '__main__':
             # limit the number of boxes
             if boxes.shape[0] > top_k:
                 boxes = boxes[0:top_k,:]
-            # get input blob
+            # extract feature with reg
             blobs = _get_blobs(im, boxes, 'reg')
-            # reshape network inputs
-            net.blobs[blob_name_ip].reshape(*(blobs[blob_name_ip].shape))
-            # forward pass
-            blobs_out = net.forward(**(blobs))
-            # copy the output
-            feat_det_reg[j-1] = np.copy(blobs_out[blob_name_op])
-        _t['det_reg'].toc()
+            net_pre_reg.blobs[blob_name_ip].reshape(*(blobs[blob_name_ip].shape))
+            blobs_out = net_pre_reg.forward(**(blobs))
+            feat_det_pre_reg[j-1] = np.copy(blobs_out[blob_name_op_reg])
+            # extract feature with ctx
+            blobs = _get_blobs(im, boxes, 'ctx')
+            net_pre_ctx.blobs[blob_name_ip].reshape(*(blobs[blob_name_ip].shape))
+            blobs_out = net_pre_ctx.forward(**(blobs))
+            feat_det_pre_ctx[j-1] = np.copy(blobs_out[blob_name_op_ctx])
+        _t['det'].toc()
 
         # save output
-        sio.savemat(feat_file, {'feat_full' : feat_full, 
-                                'feat_det_reg' : feat_det_reg})
+        sio.savemat(feat_file, {'feat_full_pre' : feat_full_pre, 
+                                'feat_full_fto' : feat_full_fto,
+                                'feat_full_ftv' : feat_full_ftv,
+                                'feat_det_pre_reg' : feat_det_pre_reg,
+                                'feat_det_pre_ctx' : feat_det_pre_ctx})
         
         _t['total'].toc()
-        print 'full: {:.3f}s det_reg: {:.3f}s total: {:.3f}s' \
-              .format(_t['full'].average_time, _t['det_reg'].average_time, 
+        print 'full: {:.3f}s det: {:.3f}s total: {:.3f}s' \
+              .format(_t['full'].average_time, _t['det'].average_time, 
                       _t['total'].average_time)
 
     print 'Done.'
